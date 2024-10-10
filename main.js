902900a93b148c5bd0ee2a1cbf0122050584e002
Martín Cristaldo <cristaldopmartin@gmail.com>
2024-10-10 00:37:33 -0300

    Agrego imágenes y modifico pronunciaciones

--- main.js
+++ main.js
@@ -7,34 +7,65 @@ const $containerMinuscula = document.querySelector(".container-minuscula");
 const $containerMayuscula = document.querySelector(".container-mayuscula");
 const $wordsSection = document.querySelector(".section-words");
 const words = {
-    "A": ["ACEITE", "ABRIL", "ADIVINAR"],
-    "B": ["BEBÉ", "BUSCAR", "BRILLANTE"],
-    "C": ["CIELO", "CORRER", "COMER"],
-    "D": ["DÍA", "DIBUJAR", "DULCE"],
-    "E": ["ELEFANTE", "ESCUCHAR", "ESCRIBIR"],
-    "F": ["FELIZ", "FUEGO", "FRUTA"],
-    "G": ["GATO", "GRANDE", "GUITARRA"],
-    "H": ["HOLA", " HACER", "HOGAR"],
-    "I": ["ISLA", "IR", "INFINITO"],
-    "J": ["JARDÍN", "JUGAR", "JOVEN"],
-    "K": ["KILO", "KARMA", "KIOSCO"],
-    "L": ["LUZ", "LEER", "LARGO"],
-    "M": ["MAR", "MESA", "MÚSICA"],
-    "N": ["NUBE", "NACER", "NOCHE"],
-    "Ñ": ["ÑANDÚ", "ÑOQUI"],
-    "O": ["OJO", "OÍR", "OSO"],
-    "P": ["PERRO", "PINTAR", "PLAYA"],
-    "Q": ["QUESO", "QUÍMICA", "QUIERO"],
-    "R": ["RÍO", "RÁPIDO", "REÍR"],
-    "S": ["SOL", "SALTAR", "SOMBRA"],
-    "T": ["TIERRA", "TOCAR", "TIEMPO"],
-    "U": ["UNIVERSO", "USAR", "UNO"],
-    "V": ["VIENTO", "VIAJAR", "VERDE"],
-    "W": ["WAFLE", "WHISKY", "WEB"],
-    "X": ["XILÓFONO", "XENÓN", "XEROGRAFÍA"],
-    "Y": ["YATE", "YOGUR", "YO"],
-    "Z": ["ZAPATO", "ZEBRA", "ZUMO"],
+    "A": ["ABRIR|./img/abrir.png", "ACEITE|./img/aceite.png"],
+    "B": ["BANDERA|./img/bandera.png", "BANANA|./img/banana.svg"],
+    "C": ["CASA|./img/casa.png", "CORAZON|./img/corazon.png"],
+    "D": ["DADO|./img/dado.png", "DENTISTA|./img/dentista.png"],
+    "E": ["ELEFANTE|./img/elefante.png", "ESCOBA|./img/escoba.png"],
+    "F": ["FLOR|./img/flor.png", "FUEGO|./img/fuego.png"],
+    "G": ["GATO|./img/gato.png", "GUITARRA|./img/guitarra.png"],
+    "H": ["HUEVO|./img/huevo.jpeg", "HELADO|./img/helado.jpeg"],
+    "I": ["IGLESIA|./img/iglesia.png", "IGUANA|./img/iguana.jpg"],
+    "J": ["JIRAFA|./img/jirafa.png", "JUGO|./img/jugo.png"],
+    "K": ["KIOSCO|./img/kiosco.jpg", "KETCHUP|./img/ketchup.png"],
+    "L": ["LÁPIZ|./img/lapiz.png", "LIMÓN|./img/limon.jpeg"],
+    "M": ["MESA|./img/mesa.png", "MARTILLO|./img/martillo.png"],
+    "N": ["NUBE|./img/nube.jpg", "NIEVE|./img/nieve.png"],
+    "Ñ": ["ÑANDÚ|./img/ñandu.png", "ÑOQUI|./img/ñoqui.jpg"],
+    "O": ["OJO|./img/ojo.png", "OVEJA|./img/oveja.png"],
+    "P": ["PERRO|./img/perro.png", "PANTALÓN|./img/pantalon.png"],
+    "Q": ["QUESO|./img/queso.png", "QUEJA|./img/queja.png"],
+    "R": ["RATÓN|./img/raton.png", "REGALO|./img/regalo.png"],
+    "S": ["SANDÍA|./img/sandia.png", "SUMA|./img/suma.png"],
+    "T": ["TIJERA|./img/tijera.png", "TENEDOR|./img/tenedor.png"],
+    "U": ["URGENCIA|./img/urgencia.png", "UVA|./img/uva.jpg"],
+    "V": ["VASO|./img/vaso.png", "VELA|./img/vela.jpeg"],
+    "W": ["WAFLE|./img/wafle.png", "WIFI|./img/wifi.png"],
+    "X": ["XILÓFONO|./img/xilofono.png"],
+    "Y": ["YO|./img/yo.png", "YOGUR|./img/yogur.png"],
+    "Z": ["ZAPATO|./img/zapato.jpeg", "ZEBRA|./img/zebra.png"],
 };
+
+const pronunciaciones = {
+    'A': 'a',
+    'B': 'be',
+    'C': 'ce',
+    'D': 'de',
+    'E': 'e',
+    'F': 'efe',
+    'G': 'ge',
+    'H': 'hache',
+    'I': 'i',
+    'J': 'jota',
+    'K': 'ka',
+    'L': 'ele',
+    'M': 'eme',
+    'N': 'hene',
+    'Ñ': 'eñe',
+    'O': 'o',
+    'P': 'pe',
+    'Q': 'cu',
+    'R': 'erre',
+    'S': 'ese',
+    'T': 'te',
+    'U': 'u',
+    'V': 'vee',  
+    'W': 'doble vee',  
+    'X': 'equis',
+    'Y': 'i griega',  
+    'Z': 'zeta'
+};
+  
 let i = 0;
 
 function textWVoice(text) {
@@ -80,15 +111,19 @@ document.addEventListener("click", (e) => {
         $containerMinuscula.dataset.letter = letter.toLowerCase();
         renderWords(letter);
         i++;
-        (i == 1) ? speakLetter('Esta es la letra ' + letter) : speakLetter(letter);
+        (i == 1) ? speakLetter('Esta es la letra ' + pronunciaciones[letter]) : speakLetter(pronunciaciones[letter]);
     }
 
     if (e.target.matches('.container-letter') || e.target.matches('.letter')) {
-        speakLetter(e.target.dataset.letter);
+        speakLetter(pronunciaciones[e.target.dataset.letter.toUpperCase()]);
     }
 
     if (e.target.matches(".word")) {
         speakLetter(e.target.textContent);
+    }
+
+    if (e.target.matches(".img-word")) {
+        speakLetter(e.target.alt);
     }
 })
 
@@ -96,9 +131,10 @@ document.addEventListener("click", (e) => {
 function renderWords(letter) {
     $wordsSection.innerHTML = "";
     words[letter].forEach(word => {
-    let $wordP = document.createElement("p");
-    $wordP.textContent = word;
-    $wordP.className = "word";
-    $wordsSection.appendChild($wordP);
+    const html = `<div style='display:flex; align-items: center;'>
+        <img src="${word.split('|')[1]}" class="img-word" style='height: 120px; width: 110px; display: inline;' alt="${word.split('|')[0]}">
+        <p class='word'>${word.split('|')[0]}</p>
+    </div>`;
+    $wordsSection.insertAdjacentHTML("beforeend", html);
    })
 }
